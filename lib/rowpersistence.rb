831b66c8440cc0b74ab7e313d5890d558a31bfe4
Ricky Hougland <houglande@gmail.com>
2015-11-04 15:40:30 -0800

    add row persistence class

---
+++ lib/rowpersistence.rb
@@ -0,0 +1,27 @@
+require "./database.rb"
+
+module TaskList
+  class RowPersistence < TaskList::Database
+
+    def initialize(db_name)
+      #not sure if it's passing an instance of database class the correct way
+
+      @db = SQLite3::Database.new(db_name)
+    end
+
+    def new_task(name, description=nil, done_date=nil)
+
+      @db.execute('
+      INSERT INTO tasks (name, description, done_date)
+      Values(?, ?, ?, ?)
+      ;' name, description, done_date)
+    end
+
+    def existing_task
+    end
+
+    def show_tasks
+    end
+    
+  end
+end
--- tasks-list.rb
+++ tasks-list.rb
@@ -2,7 +2,12 @@ require "sinatra"
 
 class TasksList < Sinatra::Base
 
+  def current_db
+    @curr_db ||= TaskList::Database.new("allofthetasks.db")
+  end
+
   get "/" do
+    current_db
     erb :index
   end
 
