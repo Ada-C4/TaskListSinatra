de4525438467fa9c08988bf1fbd3af95c0af4ddf
Brittany <BrittanyKohler@gmail.com>
2015-11-04 15:43:53 -0800

    Adding create_task method in database.rb

--- lib/database.rb
+++ lib/database.rb
@@ -10,11 +10,20 @@ module TaskList
     def create_schema
       @db.execute('
       CREATE TABLE tasks (
-       id INTEGER PRIMARY KEY,
-       name TEXT NOT NULL,
-       description TEXT NULL,
-       completed TEXT NULL
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      description TEXT NULL,
+      completed TEXT NULL
       );')
     end
+
+    def create_task(task_name, task_descr, task_comp)
+      @db.execute('
+      INSERT INTO tasks (name, description, completed)
+      VALUES(?, ?, ?)
+      );' task_name, task_descr, task_comp)
+
+    end
+
   end
 end
