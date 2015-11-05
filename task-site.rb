7b6c9eaeb81b625ff25ee54ea45fc0d80469d679
Amy Hunter <amyrhunter@gmail.com>
2015-11-05 09:35:14 -0800

    Add routes for index and get and post new pages

--- task-site.rb
+++ task-site.rb
@@ -3,8 +3,26 @@ require "./lib/database"
 
 class TaskSite < Sinatra::Base
 
+  def current_db
+    @curr_db ||= TaskList::Database.new("tasks.db")
+  end
+
   get "/" do
+    @tasks = current_db.get_tasks
     erb :index
   end
+
+  get "/new" do
+    erb :new
+  end
+
+  post "/new" do
+    @name = params[:name]
+    @description = params[:description]
+    current_db.create_tasks(@name, @description)
+    erb :index
+  end
+
+
 
 end
