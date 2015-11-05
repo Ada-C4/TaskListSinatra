65fcbb6031c966f7bec4b7800d9b1a6bf5b0348c
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-05 10:34:20 -0800

    Task form works, can add tasks to database, yayyyyy

--- task-list.rb
+++ task-list.rb
@@ -7,10 +7,9 @@ class	TaskSite < Sinatra::Base
 		@curr_db ||= TaskList::TaskCreator.new("task-list.db")
 	end
 
-	get '/' do
+	def current_list
 		task_array = current_db.get_tasks
 		@all_tasks = []
-
 		task_array.each do |task|
 			one_task = {
 				id: task[0],
@@ -20,12 +19,19 @@ class	TaskSite < Sinatra::Base
 			}
 			@all_tasks.push(one_task)
 		end
+	end
 
+	get '/' do
+		current_list
     erb :index
 	end
 
 	post '/' do
-		
+		task_name = params[:name]
+		task_descr = params[:description]
+		current_db.create_task(task_name, task_descr)
+		current_list
+		erb :index
 	end
 
 	get "/add_task" do
