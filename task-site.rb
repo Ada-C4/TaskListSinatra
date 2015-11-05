00c268c3ffbf932f17a0a302024890bf95d0392c
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-05 10:17:23 -0800

    working form and index with list of tasks

--- task-site.rb
+++ task-site.rb
@@ -13,4 +13,17 @@ class TaskSite < Sinatra::Base
     erb :index
   end
 
+  get "/new" do
+    erb :new
+  end
+
+  post "/new" do
+    @new_name = params[:name]
+    @new_descr = params[:description]
+    @new_date = params[:date_completed]
+    current_db.create_task(@new_name, @new_descr, @new_date)
+    @tasks = current_db.get_tasks
+    erb :index
+  end
+
 end
