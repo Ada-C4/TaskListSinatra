f465a2e61d1e7b19a5a8beae0a233688fd2c3606
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-05 09:43:17 -0800

    We can create tasks, select all tasks, and have the functionality seperated between classes, wahoo

--- lib/task_creator.rb
+++ lib/task_creator.rb
@@ -1,9 +1,22 @@
 require './lib/database.rb'
 
 module TaskList
-
   class	TaskCreator < Database
   	
+  	def create_task(task_name, description = nil, com_date = nil)
+  		@db.execute('
+  			INSERT INTO tasks (name, description, completed_date)
+  			VALUES(?, ?, ?)
+  			;', task_name, description, com_date)
+  	end
+
+  	def get_tasks
+  		@db.execute('
+  			SELECT name, description, completed_date
+  			FROM tasks;
+  			')
+  	end
+
   end
 
 end
