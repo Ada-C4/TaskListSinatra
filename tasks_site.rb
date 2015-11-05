4cd678089b63ca43358d88cd00f7c832fb6dc516
Wesley Willis <haileyw@gmail.com>
2015-11-05 11:35:33 -0800

    add css styling - non-functional at this point

--- tasks_site.rb
+++ tasks_site.rb
@@ -7,20 +7,24 @@ end
 
 class Tasks < Sinatra::Base
   get "/" do
+    @title = "Your Task List"
     @tasks = current_db.get_tasks
     erb :index
   end
 
   post "/" do
+    @title = "Your Task List"
     @tasks = current_db.get_tasks
     erb :index
   end
 
   get "/new" do
+    @title = "Add a new task"
     erb :new
   end
 
   post "/new" do
+    @title = "Add a new task"
     @new_task = params[:task]
     @descr = params[:description]
     @date = params[:date]
