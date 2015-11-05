2e7d4babfe4602ac862f7bad64940fe410e525f1
Claire <ClaireS29@gmail.com>
2015-11-05 09:32:48 -0800

    Added ./new and logic for index page display

--- task-site.rb
+++ task-site.rb
@@ -3,7 +3,12 @@ require "./lib/database"
 
 class TaskList < Sinatra::Base
 
+  def current_db
+    @curr_db ||= TaskList::Database.new("taskbase.db")
+  end
+
   get "/" do
+    @tasks = current_db.select_tasks
     erb :index
   end
 
