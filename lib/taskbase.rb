c2e080e00e743f448b1a53dba817983650532a1c
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-05 09:47:18 -0800

    Added create and get methods for TaskBase but they don't work

--- lib/taskbase.rb
+++ lib/taskbase.rb
@@ -1,13 +1,20 @@
 require "sqlite3"
-require "./database"
+require "./lib/database"
 
 module TaskList
   class TaskBase < TaskList::Database
 
-    def create_task
+    def create_task(name, description, date_completed)
+      @db.execute('
+      INSERT INTO tasks (name, description, date_completed)
+      VALUES (?,?,?)
+      ;', name, description, date_completed)
     end
 
     def get_task
+      @db.execute('
+      SELECT * FROM tasks;
+      ')
     end
 
   end
