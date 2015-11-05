29c1756a0d23824120b7dc54b7f7e97c6b180092
noglows <jessica.noglows@outlook.com>
2015-11-05 09:31:52 -0800

    Updated routes - Having database issue

--- task_list.rb
+++ task_list.rb
@@ -1,8 +1,14 @@
 require "sinatra"
+require "./lib/database"
 
 class TaskList < Sinatra::Base
 
+  def current_db
+    @curr_db ||= TaskList::Database.new("tasks.db")
+  end
+
   get "/" do
+    @tasks = current_db.get_tasks
     erb :index
   end
 
@@ -11,6 +17,9 @@ class TaskList < Sinatra::Base
   end
 
   post "/new" do
+    @new_name = params[:name]
+    @descr = params[:description]
+    current_db.create_task(@new_name, @descr)
     erb :index
   end
 
