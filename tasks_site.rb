532ca187549a934151c1a62613b43b0e02ff9bf8
Wesley Willis <haileyw@gmail.com>
2015-11-05 09:36:15 -0800

    add methods to tasksite.rb

--- tasks_site.rb
+++ tasks_site.rb
@@ -1,8 +1,24 @@
 require "sinatra"
 require "./lib/database"
 
+def current_db
+  @curr_db ||= TaskList::Database.new("todo_list.db")
+end
+
 class Tasks < Sinatra::Base
   get "/" do
-    "Hello" 
+    "Hello"
+  end
+
+  get "/new" do
+    erb :new
+  end
+
+  post "/new" do
+    @new_task = parms[:task]
+    @descr = params[:description]
+    @date = params[:date]
+    current_db.create_task(@new_task, @descr, @date)
+    erb :new
   end
 end
