930fc80be58249e7ce8f2f901033d2c03838016f
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-06 22:10:15 -0800

    updated Christmas styling on other pages

--- task-site.rb
+++ task-site.rb
@@ -13,26 +13,6 @@ class TaskSite < Sinatra::Base
     erb :index
   end
 
-  get "/christmas" do
-    @tasks = current_db.get_tasks
-    erb :index_christmas
-  end
-
-  post "/christmas" do
-    @to_delete = params[:tasks_to_delete]
-    if !@to_delete.nil?
-      @to_delete.each do |task|
-        current_db.delete_task(task.to_i)
-      end
-    end
-    @to_edit = params[:task_to_edit]
-    if !@to_edit.nil?
-      redirect to("/edit?id=#{@to_edit}")
-    end
-    @tasks = current_db.get_tasks
-    erb :index_christmas
-  end
-
   post "/" do
     @to_delete = params[:tasks_to_delete]
     if !@to_delete.nil?
@@ -48,6 +28,26 @@ class TaskSite < Sinatra::Base
     erb :index
   end
 
+  get "/christmas" do
+    @tasks = current_db.get_tasks
+    erb :index_christmas
+  end
+
+  post "/christmas" do
+    @to_delete = params[:tasks_to_delete]
+    if !@to_delete.nil?
+      @to_delete.each do |task|
+        current_db.delete_task(task.to_i)
+      end
+    end
+    @to_edit = params[:task_to_edit]
+    if !@to_edit.nil?
+      redirect to("/christmas/edit?id=#{@to_edit}")
+    end
+    @tasks = current_db.get_tasks
+    erb :index_christmas
+  end
+
   get "/new" do
     erb :new
   end
@@ -59,6 +59,19 @@ class TaskSite < Sinatra::Base
     current_db.create_task(@new_name, @new_descr, @new_date)
     @tasks = current_db.get_tasks
     redirect to('/')
+  end
+
+  get "/christmas/new" do
+    erb :new_christmas
+  end
+
+  post "/christmas/new" do
+    @new_name = params[:name]
+    @new_descr = params[:description]
+    @new_date = params[:date_completed]
+    current_db.create_task(@new_name, @new_descr, @new_date)
+    @tasks = current_db.get_tasks
+    redirect to('/christmas')
   end
 
   get "/edit" do
@@ -78,6 +91,25 @@ class TaskSite < Sinatra::Base
     current_db.edit_task(@id, @edited_name, @edited_descr, @edited_date)
     @tasks = current_db.get_tasks
     redirect to('/')
+  end
+
+  get "/christmas/edit" do
+    @id = params[:id]
+    @single_task = current_db.get_single_task(@id.to_i).flatten
+    @name = @single_task[1]
+    @description = @single_task[2]
+    @date = @single_task[3]
+    erb :edit_christmas
+  end
+
+  post "/christmas/edit" do
+    @edited_name = params[:name]
+    @edited_descr = params[:description]
+    @edited_date = params[:date_completed]
+    @id = params[:id]
+    current_db.edit_task(@id, @edited_name, @edited_descr, @edited_date)
+    @tasks = current_db.get_tasks
+    redirect to('/christmas')
   end
 
 end
