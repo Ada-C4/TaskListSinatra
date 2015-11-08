95e282e9b576bdb36c66e541210a97368587b812
Brittany <BrittanyKohler@gmail.com>
2015-11-08 15:48:04 -0800

    removing pry

--- task-site.rb
+++ task-site.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 require './lib/database'
-require "pry"
 
 class TaskSite < Sinatra::Base
 
@@ -19,28 +18,16 @@ class TaskSite < Sinatra::Base
   end
 
   get "/edit" do
-    # binding.pry
     @title = "Edit your shit"
     @id = params[:id]
-    # @name = current_db.get_name(@id)
     @descr = current_db.get_descr(@id)
     erb :new
   end
 
-  # post "/edit" do
-  #   @title = "Edit your shit"
-  #   @id = params[:completed]
-  #   @name = current_db.get_name(@id)
-  #   @descr = current_db.get_descr(@id)
-  #   erb :new
-  # end
-
   post "/" do
-    # binding.pry
     @name = params[:name]
     @descr = params[:descr]
     @id = params[:id]
-    # I think the below method will work for updating once other issues are resolved
     if !@name.nil? && @id.nil?
       current_db.create_task(@name, @descr)
     elsif params[:submit] == "MARK COMPLETE"
@@ -55,12 +42,6 @@ class TaskSite < Sinatra::Base
       current_db.update_task(@name, @descr, @id)
     end
 
-    # if !params[:completed].nil?
-    #   params[:completed].each do |id|
-    #     current_db.add_completion(id)
-    #   end
-
-    # end
     @tasks = current_db.get_tasks
     erb :index
   end
