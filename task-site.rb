15049f3e7e7efb32b719d2e66b3bbe79f851d1a1
Sarah <trowbrsa@gmail.com>
2015-11-08 15:25:52 -0800

    Fixing css for buttons and links

--- task-site.rb
+++ task-site.rb
@@ -43,11 +43,11 @@ class TaskSite < Sinatra::Base
     # I think the below method will work for updating once other issues are resolved
     if !@name.nil? && @id.nil?
       current_db.create_task(@name, @descr)
-    elsif params[:submit] == "Mark complete"
+    elsif params[:submit] == "MARK COMPLETE"
       params[:completed].each do |id|
         current_db.add_completion(id)
       end
-    elsif params[:delete] == "Delete shit"
+    elsif params[:delete] == "DELETE"
       params[:completed].each do |id|
         current_db.delete_task(id)
       end
